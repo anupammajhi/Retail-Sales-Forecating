0045bc2c1157350ebcaee6ee0d367c6629fe3d64
Anupam Majhi <majhi.anupam@gmail.com>
2018-03-23 09:09:00 +0530

    Quick Commit

--- main.R
+++ main.R
@@ -642,3 +642,89 @@ plot(euq_ts)
 
 
 
+#Smoothing the series - Moving Average Smoothing
+
+w <- 0.6
+euq_smooth <- filter(euq_ts, 
+                       filter=rep(1/(2*w+1),(2*w+1)), 
+                       method='convolution', sides=2)
+
+#Smoothing left end of the time series
+
+diff <- euq_smooth[w+2] - euq_smooth[w+1]
+for (i in seq(w,1,1)) {
+    euq_smooth[i] <- euq_smooth[i+1] - diff
+}
+
+#Smoothing right end of the time series
+
+n <- length(euq_ts)
+diff <- euq_smooth[n-w] - euq_smooth[n-w-1]
+for (i in seq(n-w+1, n)) {
+    euq_smooth[i] <- euq_smooth[i-1] + diff
+}
+
+#Plot the smoothed time series
+
+timevals_in <- euq_in$Months
+
+lines(euq_smooth, col="yellow", lwd=2)
+
+
+#Trying Holt Winters
+
+plot(euq_ts)
+
+cols <- c("red", "blue", "green", "black", "grey")
+alphas <- c(0.02, 0.1, 0.3,0.5,0.8)
+labels <- c(paste("alpha =", alphas), "Original")
+for (i in seq(1,length(alphas))) {
+    euq_smoothhw <- HoltWinters(euq_ts, alpha=alphas[i],
+                                beta=FALSE, gamma=FALSE)
+    
+    lines(fitted(euq_smoothhw)[,1], col=cols[i], lwd=2)
+}
+
+legend("bottomleft", labels, col=cols, lwd=2)
+
+plot(euq_ts)
+euq_smoothhw <- HoltWinters(euq_ts, alpha=0.7,
+                            beta=FALSE, gamma=FALSE)
+
+lines(fitted(euq_smoothhw)[,1], col='green', lwd=2)
+
+vals <- as.data.frame(fitted(euq_smoothhw))
+
+euq_smoothhw_total  <- data.frame(c(euq_in[1,2], (vals[,2])))
+
+#  MA does better smoothing as compared to HW, so we will use MA smoothing.
+
+
+#Building a model on the smoothed time series using classical decomposition
+#First, let's convert the time series to a dataframe
+
+euq_smoothdf <- as.data.frame(cbind(timevals_in, as.vector(euq_smooth)))
+colnames(euq_smoothdf) <- c('Months', 'Quantity')
+
+#Now, let's fit a  model with trend and seasonality to the data
+#There appears to be little seasonality in the data. Trying various degree equations
+
+lmfit <- lm(Quantity ~  (sin(0.5*Months) * poly(Months,2) + cos(0.5*Months) * poly(Months,2))
+            *exp(0.00005*Months),data=euq_smoothdf)
+
+
+global_pred <- predict(lmfit, Months=timevals_in)
+summary(global_pred)
+
+plot(euq_ts)
+lines(timevals_in, global_pred, col='green', lwd=2)
+
+#Now, let's look at the locally predictable series
+#We will model it as an ARMA series
+
+local_pred <- euq_in$Quantity-global_pred
+plot(local_pred, col='red', type = "l")
+acf(local_pred)
+acf(local_pred, type="partial")
+armafit <- auto.arima(local_pred)
+
